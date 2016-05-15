15b3779c36ed381b424da9a8f079dc4ff6df6a28
Dave Shoreman <dshoreman@users.noreply.github.com>
2016-05-15 04:05:24 +0100

    Fix some minor (yet showstopping) bugs

--- rootscripts/configure-network.sh
+++ rootscripts/configure-network.sh
@@ -7,6 +7,17 @@ read -p "Enter hostname: " _HOSTNAME
 echo "$_HOSTNAME" > /etc/hostname
 sed -ie 's/localhost$/localhost\t'$_HOSTNAME'/g' /etc/hosts
 
+# Get the interface name
+echo
+echo "We are about to print a list of network devices"
+echo "present in your system. When prompted,  type in"
+echo "the name of your ethernet device e.g. 'enp2s0'."
+echo
+ip link
+echo
+read -p "Enter interface name: " _INTERFACE
+
 # Enable DHCP
 echo "Enabling DHCP..."
-systemctl service enable dhcpcd@enp2s0.service
+systemctl enable dhcpcd@${_INTERFACE}.service
+systemctl start dhcpcd@${_INTERFACE}.service

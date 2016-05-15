e88f0575461fdfa6cdf04614f6705d0bd076c272
Dave Shoreman <dshoreman@users.noreply.github.com>
2016-05-15 21:40:56 +0100

    Force timezone symlink to replace existing one

--- rootscripts/timezone-setup.sh
+++ rootscripts/timezone-setup.sh
@@ -2,6 +2,6 @@
 
 echo "Setting system clock and timezone..."
 
-ln -s /usr/share/zoneinfo/Europe/London /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 timedatectl --set-ntp true
 hwclock --systohc --utc

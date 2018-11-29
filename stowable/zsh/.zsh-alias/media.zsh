fda711ac3795f5c88cf495b12b163724ced83c38
Dave Shoreman <dshoreman@users.noreply.github.com>
2018-11-29 13:56:19 +0000

    Add alias to ripid for ripping Audio CDs
    Defaults to use multiple threads with albums saved by name in artist
    subdirectories. -c 0 sets encode to LAME so we get MP3s out of it.

--- stowable/zsh/.zsh-alias/media.zsh
+++ stowable/zsh/.zsh-alias/media.zsh
@@ -3,6 +3,8 @@
 alias mplaya="mplayer -novideo -vc null -vo null -noar -msgcolor"
 alias shuffle="mplayer -novideo -vc null -vo null -msgcolor -shuffle ~/music/**/*.mp3"
 
+alias ripcd="ripit -c 0 -e --threads 4 --dirtemplate '\"/$artist/$album\"' --outputdir=~/music/newly-ripped"
+
 alias yta="mpsyt set show_video 0,"
 alias ytv="mpsyt set show_video 1,"
 

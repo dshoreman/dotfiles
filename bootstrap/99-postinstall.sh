bb6ae21c0175b63896f9c4c63d144223383df81a
Dave Shoreman <dshoreman@users.noreply.github.com>
2018-12-01 08:00:27 +0000

    Add git config to stowables

--- bootstrap/99-postinstall.sh
+++ bootstrap/99-postinstall.sh
@@ -4,25 +4,9 @@ if [ -f "$BOOTSTRAP_ROOT/.installed.post" ]; then
     return
 fi
 
-_GHMAIL="$(whoami)@users.noreply.github.com"
-
 echo
 echo "Almost done! Now for some last few finishing touches..."
 echo
-echo "First off, let's get Git configured."
-
-while [ -z "$_FULLNAME" ]; do
-    read -p "Enter your name: "  _FULLNAME
-done
-
-read -p "Enter your email [$_GHMAIL]: " _TMPMAIL
-
-[ -n "$_TMPMAIL" ] && _GHMAIL="$_TMPMAIL"
-
-echo -n "Setting Git name and email..."
-git config --global user.name "$_FULLNAME"
-git config --global user.email "$_GHMAIL"
-echo " [DONE]"
 
 if [ "$OS" == "arch" ]; then
     echo -n "Installing rhythmbox-chromecast..."

b4e872aaceed4259c33c968d255bb160984e6236
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-05-12 00:02:06 +0100

    Add alias for Symfony's security checker
    Like the comment says, it's not strictly for laravel projects (ie this
    file is badly named and I'm too lazy to change it) but applies to
    basically anything using Composer.
    
    Installed with `composer global require sensiolabs/security-checker`.

--- stowable/zsh/.zsh-alias/laravel.zsh
+++ stowable/zsh/.zsh-alias/laravel.zsh
@@ -7,3 +7,8 @@ alias pa="php artisan"
 alias pas="pa serve"
 alias pat="vendor/bin/phpunit"
 alias phptag="ctags -R --fields=+aimlS --languages=php"
+
+# Success uses white text on green background which is invisible,
+# so we disable colour codes with --no-ansi.
+# Not strictly laravel specific, but other PHP stuff is here so meh.
+alias sec-check="security-checker security:check --no-ansi"

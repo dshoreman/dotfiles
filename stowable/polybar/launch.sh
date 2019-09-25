d5b1f330f5793b1f0270d0b9df3907598e3a15b4
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-09-25 16:05:31 +0100

    Create shorter bar for vertical monitor
    Removes the xkeyboard module - because, let's face it, it'll never get
    used - and moves the Pacman module to the centre of a new bar created
    solely for the right (vertical) monitor. This also uses shorter versions
    of the cpu, ram, network, date and volume modules so it can all fit.

--- stowable/polybar/launch.sh
+++ stowable/polybar/launch.sh
@@ -2,6 +2,10 @@
 
 pkill polybar
 
-for m in $(polybar -m | cut -d ":" -f1); do
-    MONITOR=$m polybar primary -r -q & disown
+while pgrep -u $UID -x polybar > /dev/null; do
+    sleep 1
 done
+
+MONITOR=DP-0 polybar primary -r & disown
+MONITOR=HDMI-0 polybar primary -r -q & disown
+polybar secondary -r & disown

9856e9a0ded49d0821ff006c915a21e53cda16f5
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-10-15 07:15:20 +0100

    Use SSL for Pacman mirrors

--- stowable/zsh/.zsh-alias/pacman.zsh
+++ stowable/zsh/.zsh-alias/pacman.zsh
@@ -1,7 +1,7 @@
 # Pacman Aliases
 
 alias packeys="sudo pacman-key --refresh-keys"
-alias pacmlist="sudo reflector --save /etc/pacman.d/mirrorlist --sort rate --verbose -p 'http' -c 'United Kingdom'"
+alias pacmlist="sudo reflector --save /etc/pacman.d/mirrorlist --sort rate --verbose -p 'https' -c 'United Kingdom'"
 
 alias pacrefresh="pacmlist && packeys"
 alias pacupdate="sudo pacman -Sy"

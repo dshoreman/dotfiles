2cfe7fb355a4a1f535cc069379379da8eed75cf5
Dave Shoreman <dshoreman@users.noreply.github.com>
2019-10-15 13:25:16 +0100

    Add aliases to make/shuffle playlist of all music

--- stowable/zsh/.zsh-alias/media.zsh
+++ stowable/zsh/.zsh-alias/media.zsh
@@ -2,6 +2,8 @@
 
 alias mplaya="mplayer -novideo -vc null -vo null -noar -msgcolor"
 alias shuffle="mplayer -novideo -vc null -vo null -msgcolor -shuffle"
+alias mkplaylist='find ~/music/ -type f -iname "*.flac" -o -iname "*.m4a" -o -iname "*.mp3" -o -iname "*.wav" -o -iname "*.wma" | \grep -v "Automatically Add to iTunes" > /tmp/playlist'
+alias shuffall="mkplaylist && mpv --no-audio-display --shuffle --playlist=/tmp/playlist"
 
 alias ripcd="ripit -c 0 -e --threads 4 --dirtemplate '\"/\$artist/\$album\"' --outputdir=~/music/newly-ripped"
 alias bandcamp-dl="youtube-dl -o '~/music/newly-ripped/%(artist)s/%(album)s/%(track_number)02d %(track)s.%(ext)s'"

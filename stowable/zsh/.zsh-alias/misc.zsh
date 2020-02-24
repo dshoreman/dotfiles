5db28f799946d8793a509befe42562c034ff6d24
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-02-24 04:41:50 +0000

    Update ZSH aliases
    Removes unused weather and wallpaper aliases, and adds two new ones for
    playing music in the background (controllable via Polybar) and quitting
    the terminal when my fingers forget I'm not in vim.

--- stowable/zsh/.zsh-alias/misc.zsh
+++ stowable/zsh/.zsh-alias/misc.zsh
@@ -1,10 +1,9 @@
 hash pipes 2>/dev/null && alias afk="pipes -t0 -t1 -t3 -r10000 -Rf100"
-alias weather="while true; do clear && curl wttr.in/taunton; sleep 600; done"
+alias :q="exit"
 
 # Display aliases
 alias 1080="xrandr --output HDMI1 --mode 1920x1080"
 alias 4k="xrandr --output HDMI1 --auto --output LVDS1 --off"
-alias wallpaperrot="while true; do feh --bg-scale pictures/wallpaper/4K/ --randomize; sleep 900; done"
 
 # IRC
 alias weechat="weechat -d ~/.config/weechat"

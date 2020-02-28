f1f4b9948c119e3163715e61051e694f32571152
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-02-28 08:19:21 +0000

    Add history substring search to up and down keys

--- stowable/zplug/.config/zplug/config.zsh
+++ stowable/zplug/.config/zplug/config.zsh
@@ -1,8 +1,16 @@
+#!/usr/bin/env zsh
+
+zplug "zsh-users/zsh-history-substring-search"
+
 # Enable automatic cd by typing only paths
 setopt auto_cd
 
 # Assign search directories for automatic cd
 cdpath=(~/dev/own)
+
+# Bind up and down to history search
+bindkey '[A' history-substring-search-up
+bindkey '[B' history-substring-search-down
 
 # Only show targets when completing make commands
 zstyle ':completion:*:*:make:*' tag-order 'targets'

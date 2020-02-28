03bfa8cb81bde05ba2f52fd03838708855c29851
Dave Shoreman <dshoreman@users.noreply.github.com>
2020-02-28 22:23:13 +0000

    Manually implement (most of) lib/directories.zsh
    There are a couple bits we don't use, and most of the ls aliases
    conflict with ones we're trying to add, so this gets more specific.

--- stowable/zsh/.config/zplug/config.zsh
+++ stowable/zsh/.config/zplug/config.zsh
@@ -18,12 +18,14 @@ export ZSH_ZCOMPDUMP="${XDG_CACHE_DIR}/zplug/zcompdump-${SHORT_HOST}-${ZSH_VERSI
 # Load base plugins for shell features
 zplug "lib/completion", from:oh-my-zsh
 zplug "lib/correction", from:oh-my-zsh
-zplug "lib/directories", from:oh-my-zsh
 zplug "lib/history", from:oh-my-zsh
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 
 # Enable automatic cd by typing only paths
 setopt auto_cd
+setopt auto_pushd
+setopt pushd_ignore_dups
+setopt pushdminus
 
 # Assign search directories for automatic cd
 cdpath=(~/dev/own)
--- stowable/zsh/.config/zplug/plugs/common.zsh
+++ stowable/zsh/.config/zplug/plugs/common.zsh
@@ -1,10 +1,6 @@
 #!/usr/bin/env zsh
 
-unalias k 2>/dev/null
-unalias l 2>/dev/null
-
 zplug "junegunn/fzf", use:"shell/*.zsh", defer:2
-zplug "supercrabtree/k"
 zplug "plugins/urltools", from:oh-my-zsh
 zplug "plugins/wd", from:oh-my-zsh
 
@@ -12,14 +8,6 @@ alias df="df -h"
 alias diff="diff -s"
 alias fuck='sudo $(fc -ln -1)'
 alias grep="grep --color"
-
-alias k="k -h"
-alias \;=k
-alias l="k"
-alias la="k -A"
-alias ll="k"
-alias lS="ls -lFSsh"
-alias tree="tree -I vendor"
 
 alias cp="cp -i"
 alias mv="mv -iv"

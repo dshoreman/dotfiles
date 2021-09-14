01f214e308a26171708e317d62c057b8cf88eb2c
Dave Shoreman <dshoreman@users.noreply.github.com>
2021-09-14 21:42:23 +0100

    Add 'vsr' alias for fresh-start vim sessions

--- stowable/zsh/.config/zplug/plugs/vim.zsh
+++ stowable/zsh/.config/zplug/plugs/vim.zsh
@@ -14,6 +14,16 @@ vimsession() {
     fi
 }
 
+vimsession-recreate() {
+    local sessdir=~/.cache/vim/sessions
+    local sessfile="$sessdir/$1.vim"
+
+    [ -f "$sessfile" ] && rm -f "$sessfile"
+
+    vimsession "$1"
+}
+
 alias v=vim
 alias vs=vimsession
+alias vsr=vimsession-recreate
 alias :q="exit"

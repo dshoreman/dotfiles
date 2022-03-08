54153836facaa5b78b31cb1904d0aeee5f6c4bd7
Dave Shoreman <dshoreman@users.noreply.github.com>
2022-03-08 00:56:30 +0000

    Remove AUR setup and first-boot "rootscripts"
    Everything done by 1st-boot.sh—and subsequently rootscripts/—is handled
    by dshoreman/dashit now so keeping it here would only complicate things.

--- rootscripts/create-user.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Creating primary user..."
-
-read -p "Enter a username: " _USERNAME
-echo "$_USERNAME ALL=(ALL) ALL" > /etc/sudoers.d/$_USERNAME
-
-useradd -mg users -G games,uucp,systemd-journal,wheel $_USERNAME
-passwd $_USERNAME

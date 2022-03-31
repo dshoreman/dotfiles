5831a368e1e1f76158ce858b9d01125b94db7e26
Dave Shoreman <dshoreman@users.noreply.github.com>
2022-03-31 11:36:04 +0100

    Move stow script, add more gnome settings
    Adds dconf values for Rhythmbox, the GTK file chooser, and VirtManager.
    Also moves the stow script into stowable/scripts ready for cleanup,
    purely so that Git will actually track it as a rename not a rewrite.

--- dashit/firstboot.user.bash
+++ dashit/firstboot.user.bash
@@ -1,22 +1,50 @@
 #!/usr/bin/env bash
 
+gset() {
+    schema="${1//chooser/org.gtk.Settings.FileChooser}"
+    schema="${schema//rhythm/org.gnome.rhythmbox}"
+    schema="${schema//meld/org.gnome.meld}"
+    schema="${schema//vm/org.virt-manager.virt-manager}"
+
+    echo -n "  Setting value of '$1' in $schema to $2... "
+    gsettings set "$schema" "$2" "$3" && echo "Done!"
+}
+
 echo -n "Adding web2mpv handler..."
 mimelist="$HOME/.config/mimeapps.list"
 [ -f "${mimelist}" ] && grep -q "web2mpv" "${mimelist}" || echo "x-scheme-handler/mpv=web2mpv.desktop;" >> "${mimelist}"
 echo " [DONE]"
 
-set_meld() {
-    echo -n "Setting meld config key '$1' to $2... "
-    gsettings set org.gnome.meld "$1" "$2" && echo "Done!"
-}
-set_meld use-system-font false
-set_meld custom-font "'TerminessTTF Nerd Font Mono Medium 10'"
-set_meld statusbar-visible false
-set_meld style-scheme "'oblivion'"
-set_meld folder-status-modifiers "['new', 'modified']"
-set_meld folder-time-resolution 1
-set_meld highlight-current-line true
-set_meld highlight-syntax true
-set_meld show-line-numbers true
-set_meld indent-width 4
-set_meld indent-spaces-instead-of-tabs true
+echo; echo "Configuring GTK file chooser..."
+gset chooser date-format with-time
+gset chooser sort-column modified
+gset chooser sort-order descending
+gset chooser startup-mode cwd
+gset chooser type-format mime
+
+echo; echo "Configuring Rhythmbox..."
+gset rhythm time-display false
+gset rhythm.player transition-time 3
+gset rhythm.player use-xfade-backend true
+gset rhythm.sources browser-views genres-artists-albums
+
+echo; echo "Configuring Meld..."
+gset meld use-system-font false
+gset meld custom-font "'TerminessTTF Nerd Font Mono Medium 10'"
+gset meld statusbar-visible false
+gset meld style-scheme "'oblivion'"
+gset meld folder-status-modifiers "['new', 'modified']"
+gset meld folder-time-resolution 1
+gset meld highlight-current-line true
+gset meld highlight-syntax true
+gset meld show-line-numbers true
+gset meld indent-width 4
+gset meld indent-spaces-instead-of-tabs true
+
+echo; echo "Configuring Virt Manager..."
+gset vm xmleditor-enabled true
+gset vm.confirm forcepoweroff false
+gset vm.confirm removedev false
+gset vm.confirm unapplied-dev false
+gset vm.connections autoconnect "['qemu:///system']"
+gset vm.connections uris "['qemu:///system']"

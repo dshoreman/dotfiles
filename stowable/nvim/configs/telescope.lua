78e563f094ede0c331b2572f17bf5d4062bf13ca
Dave Shoreman <dshoreman@users.noreply.github.com>
2023-02-01 21:13:40 +0000

    Set some fairly decent defaults for Telescope
    Switches the default layout to flex which will auto-switch between
    horizontal or vertical layouts depending on how wide the editor is, as
    well as setting various other defaults and capping previews at ~2MB.
    
    That may be an overly amitious limit, I haven't tested it so we'll see.

--- stowable/nvim/configs/telescope.lua
+++ stowable/nvim/configs/telescope.lua
@@ -1,4 +1,17 @@
 local telescope, builtin = require('telescope'), require('telescope.builtin')
+local maxPreviewSize, previewers = 2048, require('telescope.previewers')
+local escToClose = {i = {["<Esc>"] = "close"}}
+
+local previewer = function(filepath, buffer, opts)
+  opts = opts or {}
+
+  filepath = vim.fn.expand(filepath)
+  vim.loop.fs_stat(filepath, function(_, stat)
+    if stat and stat.size < (maxPreviewSize * 1024) then
+      previewers.buffer_previewer_maker(filepath, buffer, opts)
+    end
+  end)
+end
 
 function rgCmd(opts)
   local cmd = {
@@ -12,14 +25,48 @@ end
 
 telescope.setup{
   defaults = {
+    buffer_previewer_maker = previewer,
     dynamic_preview_title = true,
+    layout_strategy = 'flex',
+    layout_config = {
+      scroll_speed = 1,
+      bottom_pane = {
+        prompt_position = 'bottom',
+        height = 0.7,
+      },
+      vertical = {
+        preview_cutoff = 20,
+        preview_height = 0.6,
+      },
+      flex = {
+        flip_columns = 180,
+        horizontal = {
+          preview_cutoff = 180,
+        },
+        vertical = {
+          preview_cutoff = 20,
+        },
+      },
+    },
+    mappings = {
+      i = {
+        ['<C-j>'] = 'cycle_history_next',
+        ['<C-k>'] = 'cycle_history_prev',
+      },
+    },
     vimgrep_arguments = rgCmd({
       'no-heading', 'with-filename', 'column', 'line-number'
     }),
+    winblend = 30,
   },
   pickers = {
+    commands = {
+      mappings = escToClose,
+      theme = 'dropdown',
+    },
     find_files = {
       find_command = rgCmd({'files', 'no-ignore-vcs'}),
+      path_display = {'smart'},
     }
   },
 }
@@ -37,7 +84,7 @@ function fuzzyFindFiles()
 end
 
 vim.keymap.set('n', '<C-g>', '<cmd>lua fuzzyFindFiles{}<cr>', {})
-vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files path_display={"smart"}<cr>', {})
+vim.keymap.set('n', '<C-p>', '<cmd>Telescope find_files<cr>', {})
 vim.keymap.set('n', '<C-S-p>', '<cmd>Telescope commands<cr>', {})
 vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {})
 vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {})
